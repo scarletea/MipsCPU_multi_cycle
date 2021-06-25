a91a9908b1f1a75f6edb774a8b2e256238f63b9f
scarletea <1435282930@qq.com>
2021-06-25 08:08:07 +0800

    new update

--- DEFINE.v
+++ DEFINE.v
@@ -1,43 +1,26 @@
-`define RstEnable       1'b1
-`define RstDisable      1'b0
-`define ZeroWord        32'h00000000        //32位数值0
-`define WriteEnable     1'b1
-`define WriteDisable    1'b0
-`define ReadEnable      1'b1
-`define ReadDisable     1'b0
-`define AluOpBus        7:0                 //译码阶段输出aluop_o的宽度
-`define AluSelBus       2:0                 //译码阶段输出alusel_o的宽度
-`define InsValid        1'b0                //指令有效
-`define InsInvalid      1'b1
-`define True_v          1'b1                //逻辑真
-`define False_v         1'b0
-`define ChipEnable      1'b1                //芯片使能
-`define ChipDisable     1'b0
-
-
 `define EXE_ORI         6'b001101           //ori指令码
 `define EXE_NOP         6'b000000
+
 //AluOp
-`define EXE_OR_OP       8'b00100101
-`define EXE_NOR_OP      8'b00000000
-//AluSel
-`define EXE_RES_LOGIC   3'b001
+`define ALU_ADDU_OP         4'b0000
+`define ALU_SUBU_OP         4'b0001
+`define ALU_ORI_OP          4'b0010
+//`define ALU_LW_OP           4'b0011
+//`define ALU_SW_OP           4'b0100
+//`define ALU_BEQ_OP          4'b0101
+//`define ALU_JAL_OP          4'b0110
+`define ALU_ADD_OP          4'b0011
+`define ALU_SUB_OP          4'b0100
+`define ALU_AND_OP          4'b0101
+`define ALU_OR_OP           4'b0110
+`define ALU_XOR_OP          4'b0111
+`define ALU_NOR_OP          4'b1000
+`define ALU_SLT_OP          4'b1001
+`define ALU_SLTU_OP         4'b1010
+`define ALU_ADDI_OP         4'b1011
+`define ALU_ADDIU_OP        4'b1100
+`define ALU_ANDI_OP         4'b1101
+`define ALU_XORI_OP         4'b1110
 
-`define EXE_RES_NOP     3'b000
 
-
-`define InstAddrBus     31:0                //ROM地址总线宽度
-`define InstBus         31:0                //ROM数据总线宽度
-`define InstMemNum      131071              //ROM实际大小128KB
-`define InstMemLog2     17                  //ROM实际使用地址线宽度
-
-
-`define RegAddrBus      4:0                 //Regfile地址线宽度
-`define RegBus          31:0                //Regfile数据线宽度
-`define RegWidth        32                  //通用寄存器宽度
-`define DoubleRegWidth  64                  //两倍通用寄存器宽度
-`define DoubleRegBus    63:0                //两倍通用寄存器数据线宽度
-`define RegNum          32                  //通用寄存器数量
-`define RegNumLog2      5                   //寻址通用寄存器使用的地址位数
-`define NOPRegAddr      5'b00000
 
