b33524a0cceccb2861b7d9e1e466e2bd0355375f
scarletea <1435282930@qq.com>
2021-06-25 10:38:19 +0800

    update

--- EXT.v
+++ EXT.v
@@ -1,5 +1,6 @@
 `include "DEFINE.v"
-module EXT(
+module EXT_16
+(
    input  [15:0] Imm_16,
    input  [1:0]  EXTSel,
    output reg [31:0] Imm_32
@@ -10,6 +11,23 @@ module EXT(
             `EXT_UNSIGNED:    Imm_32 = {16'd0, Imm_16};
             `EXT_SIGNED:      Imm_32 = {{16{Imm_16[15]}}, Imm_16};
             `EXT_POS_H:       Imm_32 = {Imm_16, 16'd0};
+            default: ;
+         endcase
+      end
+endmodule
+
+module EXT_5
+(
+   input  [4:0] Imm_5,
+   input  [1:0]  EXTSel,
+   output reg [31:0] Imm_32
+);
+   always @(*) 
+      begin
+         case (EXTSel)
+            `EXT_UNSIGNED:    Imm_32 = {27'd0, Imm_5};
+            `EXT_SIGNED:      Imm_32 = {{27{Imm_5[4]}}, Imm_5};
+            `EXT_POS_H:       Imm_32 = {Imm_5, 27'd0};
             default: ;
          endcase
       end
